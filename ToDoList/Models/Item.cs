dee1d12309547472427d9be359cc520852ed41ab
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-28 15:40:05 -0700

    hooray
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,12 +5,14 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
+    public int Price { get; set; }
     public int Id { get; }
     private static List<Item> _instances = new List<Item> { };
 
-    public Item(string description)
+    public Item(string description, int price)
     {
       Description = description;
+      Price = price;
       _instances.Add(this);
       Id = _instances.Count;
     }
